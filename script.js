18550d286b417e7d7201d65d4a6536efb39e99e6
roopesh <roopeshy2k@gmail.com>
2021-02-18 22:53:15 -0800

    Calculator

---
+++ script.js
@@ -0,0 +1,16 @@
+function show(val)
+{
+    document.getElementById("screen").value+=val
+}
+
+function clears()
+{
+    document.getElementById("screen").value=""
+}
+
+function equalclick()
+{
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
